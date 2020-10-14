10b9ca58572d900deb510aa9279a37abb687147a
Donizete Waterkemper <dwater2@gmail.com>
2020-10-14 08:16:58 -0300

    Desafio CI

---
+++ src/sum/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	a := 5
+    b := 5
+    total := Soma(a, b)
+    if total != 10 {
+       t.Errorf("Soma incorreta, valor: %d, esperado: %d.", total, 10)
+    }
+}
